a5643f22c5253c2af868829293751eeda4d69f76
Mike Goodspeed <mgoodspeed@getcollc.com>
2011-05-15 10:43:22 +0100

    Initial Commit

---
+++ Happiness/HappinessAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HappinessAppDelegate.h
+//  Happiness
+//
+//  Created by Mike Goodspeed on 5/15/11.
+//  Copyright 2011 Mike Goodspeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
